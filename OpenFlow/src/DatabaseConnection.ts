96d4f82da712d5636ad99d0767eee9523998080c
Allan Zimmermann <snigerosten@chatportal.dk>
2019-07-07 17:03:06 +0200

    1

--- OpenFlow/src/DatabaseConnection.ts
+++ OpenFlow/src/DatabaseConnection.ts
@@ -513,7 +513,6 @@ export class DatabaseConnection {
         }
         var user: TokenUser = Crypt.verityToken(q.jwt);
         var exists = await this.query(query, { name: 1 }, 2, 0, null, q.collectionname, q.jwt);
-        this._logger.debug("[" + user.username + "][" + q.collectionname + "] query gave " + arr.length + " results " + JSON.stringify(query));
         if (exists.length == 1) {
             q.item._id = exists[0]._id;
         }
