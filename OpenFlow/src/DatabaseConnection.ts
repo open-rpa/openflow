b2b126e43242fa49ac0e31f868e0a60dbba79adb
Allan Zimmermann <snigerosten@chatportal.dk>
2019-07-20 11:12:54 +0200

    Auto parse ISO 8601 when saving

--- OpenFlow/src/DatabaseConnection.ts
+++ OpenFlow/src/DatabaseConnection.ts
@@ -796,9 +796,25 @@ export class DatabaseConnection {
      * @returns void Clean object
      */
     traversejsonencode(o) {
+        var reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+        var reMsAjax = /^\/Date\((d|-|.*)\)[\/|\\]$/;
+
         var keys = Object.keys(o);
         for (let i = 0; i < keys.length; i++) {
             let key = keys[i];
+            let value = o[key];
+            if (typeof value === 'string') {
+                var a = reISO.exec(value);
+                if (a) {
+                    o[key] = new Date(value);
+                } else {
+                    a = reMsAjax.exec(value);
+                    if (a) {
+                        var b = a[1].split(/[-+,.]/);
+                        o[key] = new Date(b[0] ? +b[0] : 0 - +b[1]);
+                    }
+                }
+            }
             if (key.indexOf('.') > -1) {
                 try {
                     // var newkey = key.replace(new RegExp('.', 'g'), '____');
