04c01d0b6695cbec22b0a85741dd981b6a24db21
Allan Zimmermann <snigerosten@chatportal.dk>
2020-04-14 14:23:56 +0200

    try parse string orderby

--- OpenFlow/src/DatabaseConnection.ts
+++ OpenFlow/src/DatabaseConnection.ts
@@ -289,7 +289,13 @@ export class DatabaseConnection {
         var mysort: Object = {};
         if (orderby) {
             if (typeof orderby === "string" || orderby instanceof String) {
-                mysort[(orderby as string)] = 1;
+                var neworderby = null;
+                try {
+                    neworderby = JSON.parse((orderby as string));
+                    mysort = neworderby;
+                } catch (error) {
+                }
+                if (neworderby == null) mysort[(orderby as string)] = 1;
             } else {
                 mysort = orderby;
             }
