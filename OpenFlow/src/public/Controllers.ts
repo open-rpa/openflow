b44781e7a9f45f85bf7f13a3a4cd783a6735a8cf
cloudhack <allan@openrpa.dk>
2023-02-03 00:57:29 +0100

    remove noderedid

--- OpenFlow/src/public/Controllers.ts
+++ OpenFlow/src/public/Controllers.ts
@@ -7651,7 +7651,6 @@ export class AgentCtrl extends entityCtrl<any> {
                 "wiq":"nodeagent", "queue":"nodeagent",
                 "gitrepo": "https://github.com/openiap/nodeagenttest.git",
                 "gitreop2": "https://github.com/openiap/nodered.git",
-                "nodered_id": WebSocketClient.instance.user.username,
                 "oidc_client_id":"application",
                 "oidc_client_secret": ""
             }
