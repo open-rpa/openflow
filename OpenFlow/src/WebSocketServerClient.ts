db59df33a0a7385631137e8c169bd90354e2d376
cloudhack <allan@openrpa.dk>
2023-02-04 18:07:11 +0100

    wrap old errors

--- OpenFlow/src/WebSocketServerClient.ts
+++ OpenFlow/src/WebSocketServerClient.ts
@@ -528,6 +528,12 @@ export class WebSocketServerClient {
                         singleresult.priority = first.priority;
                         if (singleresult.command != "ping" && singleresult.command != "pong") {
                             singleresult.Process(this).then(msg=> {
+                                if(msg.command == "error" && !msg.error && msg.data) {
+                                    msg.data = JSON.parse(msg.data);
+                                    msg.data.error = msg.data.message;
+                                    msg.data = JSON.stringify(msg.data);
+                                    // msg.error =  msg.data; // backward compaility
+                                }
                                 this.Send(msg);
                             }) .catch((error) => {
                                 Logger.instanse.error(error, span, Logger.parsecli(this));
