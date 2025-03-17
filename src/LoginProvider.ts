ee838f8eebca99d5a43151f8c1841dc328594d89
skadefro <allan@openrpa.dk>
2025-03-17 13:52:44 +0100

    add dblocked test on TokenRequests

--- src/LoginProvider.ts
+++ src/LoginProvider.ts
@@ -1267,7 +1267,10 @@ export class LoginProvider {
                         var tuser = await await Auth.Token2User(exists.jwt, span);
                         if (tuser == null) throw new Error("Access denied");
                         var user = await Logger.DBHelper.FindById(tuser._id, span);
-                        if (user.validated == true) {
+                        if (user.dblocked == true) {
+                            Logger.instanse.debug("User is DB Locked, for key " + key + " user " + user.name + " " + user._id, span, { remoteip, cls: "LoginProvider", func: "GetTokenRequest" });
+                            res.status(200).send({ message: "ok" });
+                        } else if (user.validated == true) {
                             await Logger.DBHelper.RemoveRequestTokenID(key, span);
                             Logger.instanse.debug("return jwt for " + key, span, { remoteip, cls: "LoginProvider", func: "GetTokenRequest" });
                             res.status(200).send(Object.assign(exists, { message: "ok" }));
